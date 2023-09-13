145e66464df1b38a01026ae8349c9c8326831172
Douglas Liebl <douglasliebl@outlook.com>
2023-09-13 13:58:35 -0300

    fix

--- ms-books/src/main/java/io/github/douglasliebl/msbooks/api/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package io.github.douglasliebl.msbooks.api.exception;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String s) {
-        super(s);
-    }
-
-}
--- ms-customers/src/main/java/io/github/douglasliebl/mscustomers/api/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package io.github.douglasliebl.mscustomers.api.exception;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String s) {
-        super(s);
-    }
-}
