d1da29e4a339fa0c151398fdc1192b33d11f0c15
Douglas Liebl <douglasliebl@outlook.com>
2023-09-25 12:13:54 -0300

    Adding OpenApi, logs and TravisCI config file

--- src/main/java/io/github/douglasliebl/msbooks/api/resource/BookController.java
+++ src/main/java/io/github/douglasliebl/msbooks/api/resource/BookController.java
@@ -7,8 +7,10 @@ import io.github.douglasliebl.msbooks.api.model.entity.Book;
 import io.github.douglasliebl.msbooks.api.model.entity.Loan;
 import io.github.douglasliebl.msbooks.api.service.BookService;
 import io.github.douglasliebl.msbooks.api.service.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -24,6 +26,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
+@Slf4j
 public class BookController {
 
     private final BookService bookService;
@@ -31,13 +34,17 @@ public class BookController {
     private final LoanService loanService;
 
     @PostMapping
+    @Operation(summary = "Create Book")
     public ResponseEntity createBook(@RequestBody @Valid BookDTO request) {
+        log.info("creating a book for isbn: {}", request.getIsbn());
         var book = mapper.map(bookService.save(mapper.map(request, Book.class)), BookDTO.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(book);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Find a book based on id")
     public ResponseEntity getBook(@PathVariable Long id) {
+        log.info("obtaining details for book id: {}", id);
         var book = mapper.map(bookService.getById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)), BookDTO.class);
         return ResponseEntity.status(HttpStatus.OK).body(book);
@@ -45,14 +52,18 @@ public class BookController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes a book by id")
     public void deleteBook(@PathVariable Long id) {
+        log.info("deleting book with id: {}", id);
         bookService.delete(bookService.getById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "Updates a book")
     public ResponseEntity updateBook(@PathVariable Long id,
                                      @RequestBody BookUpdateDTO request) {
+        log.info("updating book with id: {}", id);
         var actualBook = bookService.getById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         var updatedBook = bookService.update(actualBook, request);
@@ -60,6 +71,7 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all books")
     public ResponseEntity find(BookDTO book, Pageable pageRequest) {
         var result = bookService.find(mapper.map(book, Book.class), pageRequest);
         List<BookDTO> response = result
